b569b315f26fafb716cd6c8a4695497507f8f867
datntph13533 <datntph13533@fpt.edu.vn>
2022-01-12 10:04:56 +0700

    chia nhỏ thành các file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
-import "../style.css";
+import Navigo from "navigo";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import AboutPage from "./pages/about";
+import HomePage from "./pages/home";
+import NewDetail from "./pages/newsDetail";
 
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const router = new Navigo("/", { linksSelector: "a" });
+
+const print = (content) => {
+    document.getElementById("header").innerHTML = Header.render();
+    document.getElementById("content").innerHTML = content;
+    document.getElementById("footer").innerHTML = Footer.render();
+};
+
+router.on({
+    "/": () => {
+        print(HomePage.render());
+    },
+    "/about": () => {
+        print(AboutPage.render());
+    },
+    "/news/:id": ({ data }) => {
+        const { id } = data;
+        print(NewDetail.render(id));
+    },
+});
+
+router.resolve();
