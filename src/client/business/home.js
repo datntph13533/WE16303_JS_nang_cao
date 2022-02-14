ba49ebbb13d231343fa043cb62359cb147660cf4
datntph13533 <datntph13533@fpt.edu.vn>
2022-02-14 19:07:51 +0700

    update

--- src/client/business/home.js
+++ src/client/business/home.js
@@ -6,72 +6,77 @@ const HomePage = {
         return /* html */ `
         ${Header.render()}
         <main class="mt-[10px]">
+            <!-- start banner -->
             <div class="max-w-[1440px] mx-auto">
-                <div class="flex">
-                    <div class="w-[1020px] h-[500px] bg-slate-600 mr-[10px]">
-                        <div class="container">
-                            <div class="mySlides">
-                                <img src="https://picsum.photos/1020/420">
-                            </div>
-                            <div class="mySlides">
-                                <img src="https://picsum.photos/1020/420">
-                            </div>
-                            <div class="mySlides">
-                                <img src="https://picsum.photos/1020/420">
-                            </div>
-                            <div class="mySlides">
-                                <img src="https://picsum.photos/1020/420">
-                            </div>
-                            <div class="mySlides">
-                                <img src="https://picsum.photos/1020/420">
-                            </div>
-                            <div class="mySlides">
-                                <img src="https://picsum.photos/1020/420">
-                            </div>
+                <!-- start banner -->
+                <div class="w-[1440px] h-[500px]">
+                    <div class="container">
+                        <div class="mySlides">
+                            <img src="https://picsum.photos/1440/420" class="image">
+                        </div>
+                        <div class="mySlides">
+                            <img src="https://picsum.photos/1440/420" class="image">
+                        </div>
+                        <div class="mySlides">
+                            <img src="https://picsum.photos/1440/420" class="image">
+                        </div>
+                        <div class="mySlides">
+                            <img src="https://picsum.photos/1440/420" class="image">
+                        </div>
+                        <div class="mySlides">
+                            <img src="https://picsum.photos/1440/420" class="image">
+                        </div>
+                        <div class="mySlides">
+                            <img src="https://picsum.photos/1440/420" class="image">
+                        </div>
 
-                            <a class="prev" onclick="plusSlides(-1)">❮</a>
-                            <a class="next" onclick="plusSlides(1)">❯</a>
+                        <a class="prev" onclick="plusSlides(-1)">❮</a>
+                        <a class="next" onclick="plusSlides(1)">❯</a>
 
-                            <div class="row">
-                                <div class="column">
-                                    <img class="demo cursor" src="https://picsum.photos/170/80"
-                                        onclick="currentSlide(1)">
-                                </div>
-                                <div class="column">
-                                    <img class="demo cursor" src="https://picsum.photos/170/80"
-                                        onclick="currentSlide(2)">
-                                </div>
-                                <div class="column">
-                                    <img class="demo cursor" src="https://picsum.photos/170/80"
-                                        onclick="currentSlide(3)">
-                                </div>
-                                <div class="column">
-                                    <img class="demo cursor" src="https://picsum.photos/170/80"
-                                        onclick="currentSlide(4)">
-                                </div>
-                                <div class="column">
-                                    <img class="demo cursor" src="https://picsum.photos/170/80"
-                                        onclick="currentSlide(5)">
-                                </div>
-                                <div class="column">
-                                    <img class="demo cursor" src="https://picsum.photos/170/80"
-                                        onclick="currentSlide(6)">
-                                </div>
+                        <div class="row">
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(1)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(2)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(3)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(4)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(5)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(6)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(7)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(8)">
+                            </div>
+                            <div class="column">
+                                <img class="demo cursor" src="https://picsum.photos/160/80" onclick="currentSlide(9)">
                             </div>
                         </div>
                     </div>
-
-                    <div class="w-[410px] h-[500px] bg-slate-400">
-
-                    </div>
                 </div>
+                <!-- end banner -->
+
             </div>
+            <!-- end banner -->
             <div class="max-w-[1440px] mx-auto">
 
             </div>
         </main>
         ${Footer.render()}
         `;
+    },
+    afterRender() {
+
     },
 };
 
